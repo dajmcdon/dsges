76f94e8e84f5d127a900c1a4a08c6461dbe8d089
Daniel J. McDonald <dajmcdon@gmail.com>
2021-08-02 10:12:39 -0700

    revised entropy code

--- code/more-entropy.R
+++ code/more-entropy.R
@@ -9,28 +9,31 @@ load("cluster_output/SimEst/parmEst.Rdata")
 
 pvec2 <- unlist(parmEst[nrow(parmEst), -c(1:5)])
 
-n <- 30000
+n <- 3e6
 long <- generate(n, pvec)
-ns <- c(1000,2000,3000,5000,10000,15000,20000,30000)
+ns <- c(1000,2000,3000,5000,10000,15000,20000,30000,100000,150000, 200000, 300000)
 truth <- double(length(ns))
 other <- double(length(ns))
 
-negll_per_obs <- function(y, parm) {
-  simsout = gensolution(parm)
+negll_per_obs <- function(y, simsout) {
   filt = ss.model(y, simsout, output = 'll')
   return(-filt / ncol(y))
 }
 
-
+ss_true <- gensolution(pvec)
+ss_est <- gensolution(pvec2)
 for (i in seq_along(ns)) {
-  truth[i] <- negll_per_obs(long[,seq(ns[i])], pvec)
-  other[i] <- negll_per_obs(long[,seq(ns[i])], pvec2)
+  print(paste(i," of ", length(ns)))
+  truth[i] <- negll_per_obs(long[,seq(ns[i])], ss_true)
+  other[i] <- negll_per_obs(long[,seq(ns[i])], ss_est)
 }
 
 library(tidyverse)
 tib <- tibble(n = ns, true_params = truth, est_params = other) %>% 
   pivot_longer(-n, values_to = "-loglikelihood")
 ggplot(tib, aes(n, `-loglikelihood`, color = name)) +
-  geom_line() + scale_x_log10() + geom_point() + theme_bw() +
+  geom_line() + scale_x_log10(labels = scales::label_number_si()) + 
+  geom_point() + theme_bw() +
   scale_color_viridis_d(begin=.25,end=.75) +
-  theme(legend.title = element_blank())
+  theme(legend.title = element_blank()) +
+  scale_y_continuous(trans = scales::pseudo_log_trans())
