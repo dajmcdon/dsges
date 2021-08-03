7bdd1d5fde9002892e848e2d2a12ac063389b666
Daniel J. McDonald <dajmcdon@gmail.com>
2021-08-02 17:18:07 -0700

    add entropy paragraph to manuscript

--- code/more-entropy.R
+++ code/more-entropy.R
@@ -1,5 +1,6 @@
 library(QZ)
 library(FKF)
+library(tidyverse)
 source('code/functions/modelsol.R')
 source('code/functions/reparlogp.R')
 source('code/functions/SimsCodesFort.R')
@@ -9,9 +10,12 @@ load("cluster_output/SimEst/parmEst.Rdata")
 
 pvec2 <- unlist(parmEst[nrow(parmEst), -c(1:5)])
 
-n <- 3e6
+n <- 3e5
 long <- generate(n, pvec)
-ns <- c(1000,2000,3000,5000,10000,15000,20000,30000,100000,150000, 200000, 300000)
+log_scale_seq <- function(from = 1, to = 1, length.out = 1) {
+  10^seq(log10(from), log10(to), length.out = length.out)
+}
+ns <- round(log_scale_seq(1000, n, 10), -2)
 truth <- double(length(ns))
 other <- double(length(ns))
 
@@ -24,16 +28,25 @@ ss_true <- gensolution(pvec)
 ss_est <- gensolution(pvec2)
 for (i in seq_along(ns)) {
   print(paste(i," of ", length(ns)))
-  truth[i] <- negll_per_obs(long[,seq(ns[i])], ss_true)
-  other[i] <- negll_per_obs(long[,seq(ns[i])], ss_est)
+  ii <- seq(ns[i])
+  truth[i] <- negll_per_obs(long[,ii], ss_true)
+  other[i] <- negll_per_obs(long[,ii], ss_est)
 }
 
-library(tidyverse)
 tib <- tibble(n = ns, true_params = truth, est_params = other) %>% 
-  pivot_longer(-n, values_to = "-loglikelihood")
-ggplot(tib, aes(n, `-loglikelihood`, color = name)) +
-  geom_line() + scale_x_log10(labels = scales::label_number_si()) + 
-  geom_point() + theme_bw() +
+  pivot_longer(-n, values_to = "neg_loglikelihood")
+saveRDS(tib, file = "data/entropy-investigation.RDS")
+
+
+
+# -------------------------------------------------------------------------
+
+
+ggplot(tib, aes(n, neg_loglikelihood, color = name)) +
+  geom_line() + 
+  scale_x_log10(labels = scales::label_number_si()) + 
+  geom_point() + 
+  theme_bw() +
   scale_color_viridis_d(begin=.25,end=.75) +
-  theme(legend.title = element_blank()) +
-  scale_y_continuous(trans = scales::pseudo_log_trans())
+  theme(legend.title = element_blank())
+# + scale_y_continuous(trans = scales::pseudo_log_trans())
