8228bc58c500c41b9b01da2de8993c6e15ecd49f
Daniel McDonald <dajmcdon@gmail.com>
2021-10-18 13:11:18 -0700

    entropy run for 1 million

--- code/more-entropy-long.R
+++ code/more-entropy-long.R
@@ -5,7 +5,6 @@ source('code/functions/modelsol.R')
 source('code/functions/reparlogp.R')
 source('code/functions/SimsCodesFort.R')
 source('code/priorsetup.R')
-Rcpp::sourceCpp("code/kalman-like.cpp")
 load("cluster_output/StdPvec.Rdata") # pvec
 load("cluster_output/SimEst/parmEst.Rdata")
 
@@ -20,13 +19,18 @@ ns <- round(log_scale_seq(1000, n, 10), -2)
 truth <- double(length(ns))
 other <- double(length(ns))
 
+negll_per_obs <- function(y, simsout) {
+  filt = ss.model(y, simsout, output = 'll')
+  return(-filt / ncol(y))
+}
+
 ss_true <- gensolution(pvec)
 ss_est <- gensolution(pvec2)
 for (i in seq_along(ns)) {
   print(paste(i," of ", length(ns)))
   ii <- seq(ns[i])
-  truth[i] <- -1 * ssmodel_ll(long[,ii], ss_true)
-  other[i] <- -1 * ssmodel_ll(long[,ii], ss_est)
+  truth[i] <- negll_per_obs(long[,ii], ss_true)
+  other[i] <- negll_per_obs(long[,ii], ss_est)
 }
 
 tib <- tibble(n = ns, true_params = truth, est_params = other) %>% 
@@ -39,10 +43,10 @@ saveRDS(tib, file = "data/entropy-investigation-long.RDS")
 
 
 # ggplot(tib, aes(n, neg_loglikelihood, color = name)) +
-#  geom_line() + 
-#  scale_x_log10(labels = scales::label_number_si()) + 
-#  geom_point() + 
-#  theme_bw() +
-#  scale_color_viridis_d(begin=.25,end=.75) +
-#  theme(legend.title = element_blank())
+#   geom_line() + 
+#   scale_x_log10(labels = scales::label_number_si()) + 
+#   geom_point() + 
+#   theme_bw() +
+#   scale_color_viridis_d(begin=.25,end=.75) +
+#   theme(legend.title = element_blank())
 # + scale_y_continuous(trans = scales::pseudo_log_trans())
